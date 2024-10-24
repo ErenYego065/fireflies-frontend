515ecc446c7acb8e27c4270c0185313902fe72ff
abidali671 <abid.saeed.ali92@gmail.com>
2024-10-24 17:28:48 +0500

    feat: global explorer page ui

---
+++ app/content/rewards/global-explorer/PurchaseModal.tsx
@@ -0,0 +1,79 @@
+import { Button } from '@/components/ui/button';
+import { Add, Remove } from "@mui/icons-material";
+
+import React, { useMemo, useState } from 'react'
+
+interface PurchaseModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    ticketDetails: Record<string, any>
+}
+
+const PurchaseModal = ({ isOpen, onClose, ticketDetails }: PurchaseModalProps) => {
+    const [tickets, setTickets] = useState(1);
+    const totalPrice = useMemo(() => {
+        let priceByTickets = tickets * ticketDetails?.price
+
+        if (tickets > ticketDetails?.minTicketForDiscount) {
+            priceByTickets = (priceByTickets - (priceByTickets * (ticketDetails?.discountPercentage / 100)))
+        }
+        return priceByTickets
+    }, [tickets])
+
+    if (!isOpen) {
+        return null
+    }
+
+    return (
+        <div className='fixed top-0 left-0 w-full h-full bg-[#D9D9D980] p-5'>
+            <div className='w-full h-full absolute' onClick={onClose}></div>
+            <div className='w-full max-w-[420px] p-5 border bg-[#FAFAFA] border-[#CCCBCB] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg'>
+                <div className='flex flex-col gap-7 w-full'>
+                    <h2 className="text-lg font-semibold text-[#0A0A0B]">Global Explorer</h2>
+                    <div className='flex flex-col gap-3.5'>
+                        <div>
+                            <p className='text-sm text-[#5A5555]'>Each Ticket Costs:</p>
+                            <p className='text-[#5A5555] font-bold mt-1.5'>{ticketDetails?.price}</p>
+                        </div>
+                        <div>
+                            <p className='text-sm text-[#5A5555]'>Select Number of Tickets:</p>
+                            <div className='flex justify-between items-center mt-1.5'>
+                                <Button disabled={tickets === 1} onClick={() => setTickets(tickets - 1)}><Remove /></Button>
+                                <p className='text-[#000]'>{tickets}</p>
+                                <Button onClick={() => setTickets(tickets + 1)}><Add /></Button>
+                            </div>
+                        </div>
+                        <button className="bg-[linear-gradient(98.32deg,_#505D65_20.43%,#000000_100%)] text-[#FAFAFA] h-10 px-2.5 rounded-lg p-0 text-sm font-semibold">You get a 10% discount for purchasing 50+ tickets.</button>
+                    </div>
+                    <div className='w-full h-[1px] bg-[#D9D9D980]' />
+                    <div className='flex flex-col gap-1.5'>
+                        <div className='flex justify-between'>
+                            <p className="text-sm text-[#5A5555]">Ticket</p>
+                            <p className="font-bold text-[#5A5555]">{ticketDetails?.price?.toLocaleString()} FFT</p>
+                        </div>
+                        <div className='flex justify-between'>
+                            <p className="text-sm text-[#5A5555]">Number of tickets</p>
+                            <p className="font-bold text-[#5A5555]">{tickets}</p>
+                        </div>
+                        <div className='flex justify-between'>
+                            <p className="text-sm text-[#5A5555]">Subtotal</p>
+                            <p className="font-bold text-[#5A5555]">{(tickets * ticketDetails?.price).toLocaleString()} FFT</p>
+                        </div>
+                        <div className='flex justify-between'>
+                            <p className="text-sm text-[#5A5555]">Discount ({ticketDetails?.minTicketForDiscount.toLocaleString()}+)</p>
+                            <p className="font-bold text-[#5A5555]">{ticketDetails?.discountPercentage}%</p>
+                        </div>
+                        <div className='w-full h-[1px] bg-[#D9D9D980]' />
+                        <div className='flex justify-between'>
+                            <p className="text-sm text-[#5A5555]">TOTAL</p>
+                            <p className="font-bold text-[#5A5555]">{totalPrice.toLocaleString()} FFT</p>
+                        </div>
+                    </div>
+                    <Button onClick={onClose}>Purchase</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PurchaseModal;
---
+++ app/content/rewards/global-explorer/page.tsx
@@ -0,0 +1,152 @@
+"use client"
+
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import { ChevronLeft } from "@mui/icons-material";
+import React, { useEffect, useMemo, useState } from "react";
+import moment from "moment";
+import Slider from "react-slick";
+import PurchaseModal from "./PurchaseModal";
+
+
+const Rewards = () => {
+  const [ticketDetails] = useState({
+    imgUrls: [
+      "/images/rewards/global-explorer/hero-thumbnail.svg",
+      "/images/blogs/blog-bg.jpg",
+      "/images/rewards/streak/card-desktop1.svg",
+      "/images/rewards/streak/card-desktop2.svg",
+      "/images/rewards/streak/card-desktop3.svg"
+    ],
+    description: "Embark on a world tour with accommodations in top destinations",
+    max: 100,
+    totalTickets: 250,
+    totalSold: 175,
+    endTime: moment().add(5, 'days'),
+    price: 70,
+    discountPercentage: 10,
+    minTicketForDiscount: 50,
+  })
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+
+  const ticketSoldPercentage = useMemo(() => {
+    return Math.round((ticketDetails?.totalSold / ticketDetails?.totalTickets) * 100)
+  }, [ticketDetails])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const endTime = useMemo(() => {
+    const fullDate = moment(ticketDetails?.endTime);
+    return [
+      { label: "Days", value: fullDate.diff(currentTime, 'days') },
+      { label: "Hours", value: fullDate.diff(currentTime, 'hours') % 24 },
+      { label: "Minutes", value: fullDate.diff(currentTime, 'minutes') % 60 },
+      { label: "Seconds", value: fullDate.diff(currentTime, 'seconds') % 60 }
+    ];
+  }, [ticketDetails, currentTime]);
+
+  return (
+    <React.Fragment>
+      <div className="flex flex-col gap-14 bg-gradient-radial items-center">
+        <div className="container px-5">
+          <div className="flex items-center gap-2.5 mt-[76px] w-full mx-auto">
+            <ChevronLeft sx={{ height: "64px", width: "64px", color: "#5A616C", cursor: "pointer" }} />
+            <h1 className="font-[family-name:var(--font-sofia)] font-bold text-6xl text-[#5A616C]">
+              Global Explorer
+            </h1>
+          </div>
+          <div className="grid md:grid-cols-2 gap-6 mt-10">
+            <div className="w-full">
+              <Image
+                src={ticketDetails?.imgUrls[selectedImageIndex]}
+                alt="hero"
+                className="w-full aspect-[4/3] object-cover rounded-xl"
+                height={500}
+                width={700}
+              />
+              {ticketDetails?.imgUrls?.length > 1 && <div className="px-6 py-4">
+                <Slider arrows infinite={false} draggable={false} slidesToScroll={1} speed={500} slidesToShow={4}
+                  afterChange={(currentSlide) => setSelectedImageIndex(currentSlide)}>
+                  {ticketDetails?.imgUrls?.map((url, index) =>
+                    <div>
+                      <Image
+                        style={{ border: selectedImageIndex === index ? "1px solid #5A616C" : "" }}
+                        onClick={() => setSelectedImageIndex(index)}
+                        src={url}
+                        key={"thumbnail " + index}
+                        alt={"thumbnail " + index}
+                        className="w-full aspect-video object-cover rounded-md pointer"
+                        height={90}
+                        width={120}
+                      />
+                    </div>
+                  )}
+                </Slider>
+              </div>}
+            </div>
+            <div className="h-fit p-5 border border-[#CCCBCB] bg-[#FAFAFA] rounded-lg relative max-md:mt-10">
+              <button className="bg-[linear-gradient(98.32deg,_#505D65_20.43%,#000000_100%)] text-[#FAFAFA] h-10 px-2.5 rounded-lg absolute -top-16 max-md:left-0 md:right-0 text-sm font-semibold">Buy {ticketDetails?.minTicketForDiscount}+ tickets - {ticketDetails?.discountPercentage}% discount</button>
+              <h2 className="text-lg font-semibold text-[#0A0A0B]">Description</h2>
+              <p className="text-sm text-[#5A5555]">{ticketDetails?.description}</p>
+              <div className="flex flex-col gap-3.5 py-3.5">
+                <div className="flex items-center gap-1">
+                  <Image
+                    src="/images/rewards/global-explorer/baseline-person.svg"
+                    alt="person icon"
+                    width={24}
+                    height={24}
+                    className="h-6 w-6"
+                  />
+                  <p className="text-sm text-[#5A5555]">Max {ticketDetails?.max} per person</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <Image
+                    src="/images/rewards/global-explorer/ticket-fill.svg"
+                    alt="ticket icon"
+                    width={24}
+                    height={24}
+                    className="h-6 w-6"
+                  />
+                  <p className="text-sm text-[#5A5555]">{ticketDetails?.totalTickets} entries</p>
+                </div>
+              </div>
+              <h2 className="text-lg font-semibold text-[#0A0A0B]">Ends In</h2>
+              <div className="flex gap-2.5 mt-1">
+                {endTime?.map((d, i) => <div key={i} className="flex flex-col items-center gap-0.5 justify-center h-20 w-20 rounded-lg bg-[linear-gradient(25.71deg,_rgba(217,217,217,0)_20.07%,#13AFB6_116.86%)]">
+                  <p className="font-bold text-3xl text-[#5A5555]">{d?.value}</p>
+                  <p className="text-sm text-[#5A5555]">{d?.label}</p>
+                </div>)}
+              </div>
+              <p className="text-sm text-[#5A5555] mt-1">Closes on <span className="font-bold">{moment(ticketDetails?.endTime).format('MMM Do hA')}</span></p>
+              <div className="py-3.5"><h2 className="text-lg font-semibold text-[#0A0A0B]">Tickets</h2>
+
+                <div className="w-full h-4 bg-[#CCCBCB] my-1 rounded-full">
+                  <div style={{ width: ticketSoldPercentage + "%" }} className="h-full rounded-full bg-[#00ADB5]" />
+                </div>
+                <p className="text-lg text-[#5A616C]">{ticketSoldPercentage}% Sold</p>
+              </div>
+              <p className="text-[#5A5555]">Raffle will close early if all entries are sold prior to end date.</p>
+              <div className="flex flex-row items-center gap-3.5 py-3.5">
+                <p className="font-bold text-[#00ADB5] text-3xl">{ticketDetails?.price?.toLocaleString()} FFT</p>
+                <p className="text-2xl text-[#5A616C]">/ Ticket</p>
+              </div>
+              <Button size="lg" className="w-full" onClick={() => setIsModalOpen(true)} >Buy Now</Button>
+            </div>
+          </div>
+        </div>
+      </div >
+      <PurchaseModal ticketDetails={ticketDetails} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </React.Fragment>
+  )
+}
+
+export default Rewards
