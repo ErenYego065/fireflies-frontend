c5cf4bfad4a7cc9e3a3bde318a43f6d03e224ed7
abidali671 <abid.saeed.ali92@gmail.com>
2024-10-29 13:35:52 +0500

    update tab component

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,26 +1,32 @@
 "use client";
 
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { HTMLAttributes, useState } from "react";
 
 interface TabsProps {
   labels: string[];
   panels: React.ReactNode[];
+  labelsClassName?: HTMLAttributes<HTMLDivElement>["className"];
 }
 
 const Tabs = (props: TabsProps) => {
-  const { labels, panels } = props;
+  const { labels, panels, labelsClassName } = props;
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex w-fit self-center rounded-[6px] border-2 border-neutral-200 bg-neutral-200">
+      <div
+        className={clsx(
+          "flex w-fit self-center rounded-[6px] border-2 border-neutral-200 bg-neutral-200 ",
+          labelsClassName && labelsClassName,
+        )}
+      >
         {labels?.map((label, index) => (
           <div
             key={label + index}
             onClick={() => setActiveIndex(index)}
             className={clsx(
-              "min-w-24 flex-1 cursor-pointer rounded-[4px] px-2.5 py-1 shadow-sm",
+              "min-w-24 cursor-pointer rounded-[4px] px-2.5 py-1 shadow-sm",
               activeIndex === index
                 ? "bg-white text-black"
                 : "transparent text-neutral-700",
