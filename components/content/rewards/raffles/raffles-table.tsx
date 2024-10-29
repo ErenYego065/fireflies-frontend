32d82793d48778c92ea8e38f3077327d7bad1497
abidali671 <abid.saeed.ali92@gmail.com>
2024-10-29 15:17:26 +0500

    update raffles table

--- components/content/rewards/raffles/raffles-table.tsx
+++ components/content/rewards/raffles/raffles-table.tsx
@@ -43,97 +43,187 @@ const raffleData = [
 
 const RafflesTable = () => {
   return (
-    <div className="flex flex-col gap-6 pb-72">
-      <h2 className="text-secondary-500 text-[40px] font-bold">My Raffles</h2>
-      <div>
-        <div className="mb-2.5 md:mb-4">
-          <Tabs
-            labelsClassName="!self-start"
-            labels={["Active", "Past Participation", "Winnings"]}
-            panels={[]}
-          />
-        </div>
-        <div className="flex justify-between gap-2.5 max-md:flex-col">
-          <div className="flex gap-2">
-            <Button
-              size={"sm"}
-              className="border border-neutral-200 bg-white text-black"
-            >
-              <Image
-                src="/icons/filter.svg"
-                alt="filter"
-                height={16}
-                width={16}
-                className="mr-1.5"
-              />
-              Filter
-            </Button>
-            <Button
-              size={"sm"}
-              className="border border-neutral-200 bg-white text-black"
-            >
-              Action
-            </Button>
+    <div className="flex flex-col gap-16">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-secondary-500 text-[40px] font-bold">
+          Raffle History
+        </h2>
+        <div>
+          <div className="mb-2.5 md:mb-4">
+            <Tabs
+              labelsClassName="!self-start"
+              labels={["Ended", "Warnings"]}
+              panels={[]}
+            />
           </div>
-          <div className="flex gap-1.5">
-            <Input className="md:w-80" placeholder="Search by raffle name" />
-            <Button>Search</Button>
+          <div className="flex justify-between gap-2.5 max-md:flex-col">
+            <div className="flex gap-2">
+              <Button
+                size={"sm"}
+                className="border border-neutral-200 bg-white text-black"
+              >
+                <Image
+                  src="/icons/filter.svg"
+                  alt="filter"
+                  height={16}
+                  width={16}
+                  className="mr-1.5"
+                />
+                Filter
+              </Button>
+            </div>
+            <div className="flex gap-1.5">
+              <Input className="md:w-80" placeholder="Search by raffle name" />
+              <Button>Search</Button>
+            </div>
           </div>
         </div>
+        <Table>
+          <TableHeader className="[&>td]:text-primary-900 border-b-2 border-primary-500 [&>td]:text-nowrap [&>td]:text-sm [&>td]:font-semibold">
+            <TableCell>
+              <div className="flex items-center gap-1.5">
+                <Image
+                  src="/icons/compare-arrows.svg"
+                  alt="compare arrow"
+                  height={20}
+                  width={20}
+                />
+                Date
+              </div>
+            </TableCell>
+            <TableCell>
+              <div className="flex items-center gap-1.5">
+                <Image
+                  src="/icons/compare-arrows.svg"
+                  alt="compare arrow"
+                  height={20}
+                  width={20}
+                />
+                Closed Date
+              </div>
+            </TableCell>
+            <TableCell>Raffle Name</TableCell>
+            <TableCell>Ticket Price</TableCell>
+            <TableCell>Number of Tickets</TableCell>
+            <TableCell>Purchased Ticket</TableCell>
+            <TableCell>Status</TableCell>
+          </TableHeader>
+          <TableBody>
+            {raffleData?.map((data, index) => (
+              <TableRow
+                key={index}
+                className="[&>td]:text-nowrap [&>td]:text-sm [&>td]:text-black"
+              >
+                <TableCell className="font-semibold">{data?.date}</TableCell>
+                <TableCell className="font-semibold">
+                  {data?.closedDate}
+                </TableCell>
+                <TableCell className="font-semibold">
+                  {data?.raffleName}
+                </TableCell>
+                <TableCell>{data?.ticketPrice}</TableCell>
+                <TableCell>{data?.numberOfTickets}</TableCell>
+                <TableCell>
+                  {data?.purchasedTicket?.toLocaleString()} FFT
+                </TableCell>
+                <TableCell>{data?.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
-      <Table>
-        <TableHeader className="[&>td]:text-primary-900 border-b-2 border-primary-500 [&>td]:text-nowrap [&>td]:text-sm [&>td]:font-semibold">
-          <TableCell>
-            <div className="flex items-center gap-1.5">
-              <Image
-                src="/icons/compare-arrows.svg"
-                alt="compare arrow"
-                height={20}
-                width={20}
-              />
-              Date
+      <div className="flex flex-col gap-6 pb-72">
+        <h2 className="text-secondary-500 text-[40px] font-bold">My Raffles</h2>
+        <div>
+          <div className="mb-2.5 md:mb-4">
+            <Tabs
+              labelsClassName="!self-start"
+              labels={["Active", "Past Participation", "Winnings"]}
+              panels={[]}
+            />
+          </div>
+          <div className="flex justify-between gap-2.5 max-md:flex-col">
+            <div className="flex gap-2">
+              <Button
+                size={"sm"}
+                className="border border-neutral-200 bg-white text-black"
+              >
+                <Image
+                  src="/icons/filter.svg"
+                  alt="filter"
+                  height={16}
+                  width={16}
+                  className="mr-1.5"
+                />
+                Filter
+              </Button>
+              <Button
+                size={"sm"}
+                className="border border-neutral-200 bg-white text-black"
+              >
+                Action
+              </Button>
             </div>
-          </TableCell>
-          <TableCell>
-            <div className="flex items-center gap-1.5">
-              <Image
-                src="/icons/compare-arrows.svg"
-                alt="compare arrow"
-                height={20}
-                width={20}
-              />
-              Closed Date
+            <div className="flex gap-1.5">
+              <Input className="md:w-80" placeholder="Search by raffle name" />
+              <Button>Search</Button>
             </div>
-          </TableCell>
-          <TableCell>Raffle Name</TableCell>
-          <TableCell>Ticket Price</TableCell>
-          <TableCell>Number of Tickets</TableCell>
-          <TableCell>Purchased Ticket</TableCell>
-          <TableCell>Status</TableCell>
-        </TableHeader>
-        <TableBody>
-          {raffleData?.map((data, index) => (
-            <TableRow
-              key={index}
-              className="[&>td]:text-nowrap [&>td]:text-sm [&>td]:text-black"
-            >
-              <TableCell className="font-semibold">{data?.date}</TableCell>
-              <TableCell className="font-semibold">
-                {data?.closedDate}
-              </TableCell>
-              <TableCell className="font-semibold">
-                {data?.raffleName}
-              </TableCell>
-              <TableCell>{data?.ticketPrice}</TableCell>
-              <TableCell>{data?.numberOfTickets}</TableCell>
-              <TableCell>
-                {data?.purchasedTicket?.toLocaleString()} FFT
-              </TableCell>
-              <TableCell>{data?.status}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </div>
+        </div>
+        <Table>
+          <TableHeader className="[&>td]:text-primary-900 border-b-2 border-primary-500 [&>td]:text-nowrap [&>td]:text-sm [&>td]:font-semibold">
+            <TableCell>
+              <div className="flex items-center gap-1.5">
+                <Image
+                  src="/icons/compare-arrows.svg"
+                  alt="compare arrow"
+                  height={20}
+                  width={20}
+                />
+                Date
+              </div>
+            </TableCell>
+            <TableCell>
+              <div className="flex items-center gap-1.5">
+                <Image
+                  src="/icons/compare-arrows.svg"
+                  alt="compare arrow"
+                  height={20}
+                  width={20}
+                />
+                Closed Date
+              </div>
+            </TableCell>
+            <TableCell>Raffle Name</TableCell>
+            <TableCell>Ticket Price</TableCell>
+            <TableCell>Number of Tickets</TableCell>
+            <TableCell>Purchased Ticket</TableCell>
+            <TableCell>Status</TableCell>
+          </TableHeader>
+          <TableBody>
+            {raffleData?.map((data, index) => (
+              <TableRow
+                key={index}
+                className="[&>td]:text-nowrap [&>td]:text-sm [&>td]:text-black"
+              >
+                <TableCell className="font-semibold">{data?.date}</TableCell>
+                <TableCell className="font-semibold">
+                  {data?.closedDate}
+                </TableCell>
+                <TableCell className="font-semibold">
+                  {data?.raffleName}
+                </TableCell>
+                <TableCell>{data?.ticketPrice}</TableCell>
+                <TableCell>{data?.numberOfTickets}</TableCell>
+                <TableCell>
+                  {data?.purchasedTicket?.toLocaleString()} FFT
+                </TableCell>
+                <TableCell>{data?.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
