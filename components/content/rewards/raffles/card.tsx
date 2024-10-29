b11a50175d30bca3d27e06bfc106c2847154722f
abidali671 <abid.saeed.ali92@gmail.com>
2024-10-29 13:35:28 +0500

    fix: card ui

--- components/content/rewards/raffles/card.tsx
+++ components/content/rewards/raffles/card.tsx
@@ -37,52 +37,58 @@ const Card = ({ data }: any) => {
           "linear-gradient(130.82deg, #E9EBED 0%, #A9AEB7 100%)",
         border: "2px solid #CCCBCB",
       }}
-      className="relative rounded-[20px] bg-[linear-gradient(0deg,rgba(0,173,181,0.175)_-29.68%,rgba(255,255,255,0.35)_100%)] p-6"
+      className="relative flex flex-col rounded-[20px] bg-[linear-gradient(0deg,rgba(0,173,181,0.175)_-29.68%,rgba(255,255,255,0.35)_100%)] p-6"
     >
-      <Image
-        src={data?.imgUrl}
-        alt={data?.title}
-        width={320}
-        height={250}
-        className="aspect-auto w-full"
-      />
-      <div className="border-secondary-400 my-5 border-b pb-2.5">
-        <h4 className="text-2xl font-bold text-secondary-700">{data?.title}</h4>
-        <p className="mt-2.5 text-base text-secondary-700 md:text-lg">
-          {data?.description}
-        </p>
-      </div>
-      <div className="flex w-full items-center justify-between">
-        <div>
-          <h2 className="text-lg text-secondary-700">Tickets</h2>
-          <h2 className="text-lg font-bold text-secondary-900">
-            {data?.totalTickets}
-          </h2>
-        </div>
-        <div>
-          <h2 className="text-right text-lg text-secondary-700">Available</h2>
-          <h2 className="text-right text-lg font-bold text-secondary-900">
-            {data?.totalTickets - data?.totalSold}
-          </h2>
+      <div className="w-full">
+        <Image
+          src={data?.imgUrl}
+          alt={data?.title}
+          width={320}
+          height={250}
+          className="aspect-auto w-full"
+        />
+        <div className="mt-5 pb-2.5">
+          <h4 className="text-2xl font-bold text-secondary-700">
+            {data?.title}
+          </h4>
+          <p className="mt-2.5 text-base text-secondary-700 md:text-lg">
+            {data?.description}
+          </p>
         </div>
       </div>
-      <Button className="my-2.5 w-full">Buy Ticket</Button>
-      <div className="flex flex-col gap-1">
-        <div className="flex justify-between">
-          <p className="text-lg text-secondary-700">Ends In</p>
-          <p className="text-lg font-bold text-secondary-900">
-            {getCountdown()?.[1]?.value}H : {getCountdown()?.[2]?.value}M :{" "}
-            {getCountdown()?.[3]?.value}S
-          </p>
+      <div className="border-secondary-400 mt-auto w-full border-t pt-5">
+        <div className="flex w-full items-center justify-between">
+          <div>
+            <h2 className="text-lg text-secondary-700">Tickets</h2>
+            <h2 className="text-lg font-bold text-secondary-900">
+              {data?.totalTickets}
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-right text-lg text-secondary-700">Available</h2>
+            <h2 className="text-right text-lg font-bold text-secondary-900">
+              {data?.totalTickets - data?.totalSold}
+            </h2>
+          </div>
         </div>
-        <ProgressBar progress={ticketSoldPercentage} />
-        <div className="flex justify-between">
-          <p className="text-lg text-secondary-700">
-            {ticketSoldPercentage}% Sold
-          </p>
-          <p className="text-lg font-bold text-secondary-900">
-            {data?.totalSold}/{data?.totalTickets}
-          </p>
+        <Button className="my-2.5 w-full">Buy Ticket</Button>
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between">
+            <p className="text-lg text-secondary-700">Ends In</p>
+            <p className="text-lg font-bold text-secondary-900">
+              {getCountdown()?.[1]?.value}H : {getCountdown()?.[2]?.value}M :{" "}
+              {getCountdown()?.[3]?.value}S
+            </p>
+          </div>
+          <ProgressBar progress={ticketSoldPercentage} />
+          <div className="flex justify-between">
+            <p className="text-lg text-secondary-700">
+              {ticketSoldPercentage}% Sold
+            </p>
+            <p className="text-lg font-bold text-secondary-900">
+              {data?.totalSold}/{data?.totalTickets}
+            </p>
+          </div>
         </div>
       </div>
     </div>
